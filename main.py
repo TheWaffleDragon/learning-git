08f58fdc9847b46cb06894d36a3da7167586ee6c
KB <baczewska.katarzyna@gmail.com>
2022-04-15 10:44:43 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+# Zadanie 1
+
+lista = {"Piekarnia": ["chleb","bulki","paczek"], "Warzywniak" : ["marchew","seler","rukola"]}
+count = 0
+
+print("Lista zakupow:")
+for sklep, rzeczy in lista.items():
+    print(f"Idę do {sklep}, kupię tu natępujące rzeczy:{rzeczy}")
+    count += len(rzeczy)
+print(f"W sumie kupuję {count} rzeczy")    
+
+
+#%% Zadanie 2
+
+podzielne_przez_5 = []
+
+for num in range(1,101):
+    if(num%5)==0:
+        podzielne_przez_5.append(num)
+
+print(podzielne_przez_5)  
+
+print(list(map(lambda x: x**3, podzielne_przez_5)))      
